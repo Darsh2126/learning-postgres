5bc19b875f7b4602d1d241bbab5bdc65f3e6ffc2
Darsh2126 <darsh2126gajdhar@gmail.com>
2025-04-09 13:03:44 +0530

    Comment updated

--- basic-crud/task_solution.sql
+++ basic-crud/task_solution.sql
@@ -1,5 +1,7 @@
 -- Solution 1
 -- Question: Insert a new task for user 1 with title "Finish PostgreSQL practice" and description "Do all CRUD steps today".
+-- As we have linked our tasks table to users table, we can use user_id 1. so firstly create a user which is already created in crud.sql file.
+
 Insert into tasks (user_id, title, description) values(1, 'Finish PostgreSQL practice', 'Do all CRUD steps today');
 
 -- Solution 2
