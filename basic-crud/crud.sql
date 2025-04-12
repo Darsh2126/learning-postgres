6dfe14893014c37489cd5b1c276c27f5fd009a6d
Darsh2126 <darsh2126gajdhar@gmail.com>
2025-04-12 17:56:53 +0530

    Topics learned - UUID, ENUMS, JSONB and indexes

--- basic-crud/crud.sql
+++ basic-crud/crud.sql
@@ -39,3 +39,49 @@ RETURNING *;
 -- Insert a new task for user 1 with title "Finish PostgreSQL practice" and description "Do all CRUD steps today".
 -- Then, write another query to fetch only the tasks that are still pending (i.e., not marked as done) for user 1.
 -- You can find this solution ins task_solution.sql file to verify your answers
+-- Note: Returning * will be use during Insert, update and delete
+
+/*
+Next Topics:
+Step-by-Step Upgradation to our existing Schema
+1 UUIDs – Replace SERIAL IDs for safer, unique identifiers
+2 ENUMs – For fields like task priority
+3 Generated Columns – Auto-calculate values
+4 JSONB – Store flexible data like tags or metadata
+5 Partial Indexes – Index only rows that match a condition
+*/
+
+-- Use of UUIDs for IDs
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    username VARCHAR(50) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+/*
+🔍 What It Does:
+This enables the uuid-ossp extension, which gives you functions like:
+uuid_generate_v4() → generates a random UUID (version 4)
+You only need to run this once per database. After that, you can use uuid_generate_v4() freely in DEFAULT values for UUID columns.
+*/
+
+-- Use of ENUMs for Priority
+CREATE TYPE priority AS ENUM ('low', 'medium', 'high');
+
+CREATE TABLE tasks (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+    title TEXT NOT NULL,
+    description TEXT,
+    is_done BOOLEAN DEFAULT FALSE,
+    priority task_priority DEFAULT 'medium',
+    tags JSONB,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+);
+
+-- Partial Index = Only on incomplete tasks
+CREATE INDEX idx_incomplete_tasks 
+ON tasks (user_id) 
+WHERE is_done = FALSE;
--- basic-crud/task_solution.sql
+++ basic-crud/task_solution.sql
@@ -7,3 +7,26 @@ Insert into tasks (user_id, title, description) values(1, 'Finish PostgreSQL pra
 -- Solution 2
 -- Question: Get all tasks for user 1
 Select * from tasks where user_id = 1;
+
+/*
+🧠 Your Challenge (🔥 Boss Level):
+Create and run queries that do the following:
+Insert a user and a few tasks with:
+1. Random UUIDs
+2. Different priority levels
+3. Add a tags field (JSONB) like {"tags": ["postgres", "practice"]}
+
+Write a query to:
+1. Fetch only high-priority, incomplete tasks
+2. Include the user’s username
+3. Filter by a specific tag (e.g., tags @> '{"tags": ["postgres"]}')
+4. Sort by created_at descending
+*/
+
+-- Solution
+SELECT * FROM tasks 
+WHERE 
+  priority = 'high'
+  AND is_done = FALSE
+  AND tags @> '{"tags": ["postgres"]}'
+  order by created_at desc;
